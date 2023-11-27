4e74cb5d192f3b5346335564b18ba1de3259027b
Steve2100 <113709258+Steve2100@users.noreply.github.com>
2023-11-27 20:48:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/animalv1/anima_diet.java
@@ -0,0 +1,14 @@
+package com.example.animalv1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class anima_diet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anima_diet);
+    }
+}
